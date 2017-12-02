6b39caffa99ffd018b97e9262920074fb06984db
0518taka9 <snufkingdom96@gmail.com>
2017-12-02 21:20:04 +0900

    log_writer.pyにグラフ描画を実装

--- log_writer.py
+++ log_writer.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from bitflyer import *
+from lib import Sequence, Drawer
 import time
 
 API_KEY = ""
@@ -9,6 +10,17 @@ API_SECRET = ""
 last_id = 0
 last_action = time.time()
 wait = 60
+
+N_CURVE = 4  # 曲線の数
+WIDTH = 60  # 表示するデータ数
+
+first_day = True
+
+L = 34
+
+shortEMA = Sequence(L)  # 5EMA
+middleEMA = Sequence(L)  # 13EMA
+longEMA = Sequence(L)  # 34EMA
 
 if __name__ == '__main__':
 
@@ -20,6 +32,10 @@ if __name__ == '__main__':
         # (wait)秒に1回実行
         if time.time() - last_action >= wait:
             last_action = time.time()
+
+            """
+            ログデータをファイルに出力
+            """
 
             # tickerを取得
             ticker = api.get_ticker(product)
@@ -52,7 +68,41 @@ if __name__ == '__main__':
             else:
                 average = s_price / s_amount
 
-            # (終値, 平均値, 数量, 高値, 安値)
+            # (終値, 平均値, 数量, 高値, 安値)をlog.txtに出力
             f = open("log.txt", "a")
-            f.write("(" + str(ticker["ltp"]) + ", " + str(average) + ", " + str(s_amount) + ", " + str(ticker["best_ask"]) + ", " + str(ticker["best_bid"]) + ")" + "\n")
+            f.write("(" + str(ticker["ltp"]) + ", " + str(average) + ", " + str(s_amount) + ", " + str(
+                ticker["best_ask"]) + ", " + str(ticker["best_bid"]) + ")" + "\n")
             f.close()
+
+            """
+            グラフ描画
+            """
+            drawer = Drawer((N_CURVE, WIDTH))
+
+            # 1日目は移動平均に終値を用いる
+            if first_day:
+                short = average
+                middle = average
+                long_ = average
+
+                shortEMA.append(short)
+                middleEMA.append(middle)
+                longEMA.append(long_)
+
+                first_day = False
+            else:
+                # 2日目以降
+                # EMA(n) = EMA(n－1) + α ×｛当日価格 - EMA(n-1)｝
+                # α（平滑化定数）＝2 / (n＋1）
+                short = shortEMA.get(-1) + (2.0 / 6.0) * (average - shortEMA.get(-1))
+                middle = middleEMA.get(-1) + (2.0 / 14.0) * (average - middleEMA.get(-1))
+                long_ = longEMA.get(-1) + (2.0 / 35.0) * (average - longEMA.get(-1))
+
+                shortEMA.append(short)
+                middleEMA.append(middle)
+                longEMA.append(long_)
+
+            data = (average, short, middle, long_)
+
+            drawer.update(data)
+            drawer.sleep(0.1)
